947622346ae5ad3dc3f767eb29d3fe15ce7c6042
matthewogtong <ogtongm@gmail.com>
2021-01-17 17:21:45 -0500

    commit tree fbc4c6b15906e782706485e31254c6c7599977bb

--- src/Article.js
+++ src/Article.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Article() {
+const Article = () => {
   return (
-    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
-  );
+    <div>Dear Reader: Bjarne Strouststrup has the perfect lecture oration.</div>
+  )
 }
 
 export default Article;
